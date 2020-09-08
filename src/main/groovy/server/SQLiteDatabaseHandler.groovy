bc691fc78bcf8fddf1dda99971ab9d739a149f14
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-09-08 19:42:17 +0700

    SQLiteDatabaseHandler rf

--- src/main/groovy/server/SQLiteDatabaseHandler.groovy
+++ src/main/groovy/server/SQLiteDatabaseHandler.groovy
@@ -3,13 +3,13 @@ package server
 import exceptions.ProcessingException
 import groovy.transform.CompileStatic
 
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Statement
-
-import static Server.SITE_NAME
 
 @CompileStatic
 class SQLiteDatabaseHandler {
@@ -18,38 +18,23 @@ class SQLiteDatabaseHandler {
     private static final String DB_PREFIX = "jdbc:sqlite:"
     private static final String DB_URL = "$DB_PREFIX$DB_DIRECTORY$DB_FILENAME"
 
-    private Connection connection
-
     SQLiteDatabaseHandler() {
-        File dir = new File(DB_DIRECTORY)
-        if (!dir.exists()) {
-            dir.mkdir()
-        }
+        Files.createDirectories(Paths.get(DB_DIRECTORY))
 
-        connect() // TODO private method execute()
-        try (Statement statement = connection.createStatement()) {
+        executeStatement({ Statement statement ->
             statement.execute("""
                 CREATE TABLE IF NOT EXISTS $PathTable.NAME (
                     $PathTable.Cols.PATH TEXT NOT NULL,
                     UNIQUE($PathTable.Cols.PATH)
                 );
             """)
-        } catch (SQLException e) {
-            throw new ProcessingException("Unable to create or open database")
-        } finally {
-            disconnect()
-        }
-
-        /*try { TODO delete
-            insertPath(SITE_NAME)
-        } catch (ProcessingException ignored) {}*/
+        })
     }
 
     Collection<String> getPaths() {
         ArrayList<String> paths = new ArrayList<>()
 
-        connect() // TODO private method
-        try (Statement statement = connection.createStatement()) {
+        executeStatement({ Statement statement ->
             ResultSet resultSet = statement.executeQuery("""
                 SELECT $PathTable.Cols.PATH FROM $PathTable.NAME;
             """)
@@ -57,56 +42,36 @@ class SQLiteDatabaseHandler {
             while (resultSet.next()) {
                 paths.add(resultSet.getString(PathTable.Cols.PATH))
             }
-        } catch (SQLException e) {
-            throw new ProcessingException(e.getMessage())
-        } finally {
-            disconnect()
-        }
+        })
 
         paths
     }
 
     void insertPath(String path) {
-        connect() // TODO private method
-        try (Statement statement = connection.createStatement()) {
+        executeStatement({ Statement statement ->
             statement.execute("""
                 INSERT INTO $PathTable.NAME ($PathTable.Cols.PATH)
                 VALUES('$path');
             """)
-        } catch (SQLException e) {
-            throw new ProcessingException(e.getMessage())
-        } finally {
-            disconnect()
-        }
+        })
     }
 
     void deletePath(String path) {
-        connect() // TODO private method
-        try (Statement statement = connection.createStatement()) {
+        executeStatement({ Statement statement ->
             statement.execute("""
                 DELETE FROM $PathTable.NAME WHERE $PathTable.Cols.PATH = '$path';
             """)
+        })
+    }
+
+    private void executeStatement(Closure closure) {
+        try (
+            Connection connection = DriverManager.getConnection(DB_URL);
+            Statement statement = connection.createStatement()
+        ) {
+            closure.call(statement)
         } catch (SQLException e) {
             throw new ProcessingException(e.getMessage())
-        } finally {
-            disconnect()
-        }
-    }
-
-    private void connect() {
-        try {
-//            Class.forName("org.sqlite.JDBC") // TODO delete or replace?
-            connection = DriverManager.getConnection(DB_URL)
-        } catch (SQLException/* | ClassNotFoundException*/ e) { // TODO delete
-            e.printStackTrace()
-        }
-    }
-
-    private void disconnect() {
-        try {
-            connection.close()
-        } catch (SQLException e) {
-            e.printStackTrace()
         }
     }
 
